fd9a0a7a59521669bcd6081064bad133cd5a7a79
gerardo <gerardomalpartidavela@gmail.com>
2018-10-05 13:03:24 -0700

    adding some lines from borderlands to ex00

---
+++ d03/ex00/FragTrap.hpp
@@ -0,0 +1,40 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include <iostream>
+# include <string>
+# include <cstdlib>
+# include <time.h>
+
+class FragTrap
+{
+	private:
+		static const std::string	_melee_atacks[5];
+		static const std::string	_ranged_attacks[5];
+		static const std::string	_semi_random_attacks[5];
+		static bool					_time_rnd_set;
+		std::string					_Name;
+		int							_Hit_Points;
+		int							_Energy_Points;
+		int							_Lv;
+		int							_Melle_atck_dmg;
+		int							_Ranged_atck_dmg;
+		int							_Max_EP;
+		int							_Max_HP;
+		int							_Armor_dmg_reduc;
+
+	public:
+		FragTrap();
+		FragTrap(std::string name);
+		FragTrap(FragTrap const &copy);
+		~FragTrap();
+		FragTrap					&operator = (const FragTrap &data);
+
+		void						rangedAttack(std::string const & target);
+		void						melleaAttack(std::string const & target);
+		void						takeDamage(unsigned int amount);
+		void						beRepaired(unsigned int amount);
+		void						vaulthunter_dot_exe(std::string const &target);
+};
+
+#endif
---
+++ d03/ex00/main.cpp
@@ -0,0 +1,44 @@
+#include "FragTrap.hpp"
+
+int	main(void)
+{
+	FragTrap		test("Lili");
+	FragTrap		*ptr = new FragTrap("Nicholas");
+	FragTrap		b = test;
+	FragTrap		d(test);
+
+	test.vaulthunter_dot_exe("Nicholas");
+	test.vaulthunter_dot_exe("Nicholas");
+	test.vaulthunter_dot_exe("Nicholas");
+	test.vaulthunter_dot_exe("Nicholas");
+	test.vaulthunter_dot_exe("Nicholas");
+	test.takeDamage(60);
+	test.beRepaired(30);
+	std::cout << std::endl;
+	ptr->rangedAttack("Lili");
+	ptr->rangedAttack("Lili");
+	ptr->melleaAttack("Lili");
+	ptr->melleaAttack("Lili");
+	ptr->vaulthunter_dot_exe("Lili");
+	ptr->vaulthunter_dot_exe("Lili");
+	ptr->vaulthunter_dot_exe("Lili");
+	ptr->takeDamage(60);
+	ptr->beRepaired(30);
+	std::cout << std::endl;
+	b.vaulthunter_dot_exe("Nicholas");
+	b.vaulthunter_dot_exe("Nicholas");
+	b.vaulthunter_dot_exe("Nicholas");
+	b.vaulthunter_dot_exe("Nicholas");
+	b.vaulthunter_dot_exe("Nicholas");
+	b.takeDamage(60);
+	b.beRepaired(30);
+	std::cout << std::endl;
+	d.vaulthunter_dot_exe("Nicholas");
+	d.vaulthunter_dot_exe("Nicholas");
+	d.vaulthunter_dot_exe("Nicholas");
+	d.vaulthunter_dot_exe("Nicholas");
+	d.vaulthunter_dot_exe("Nicholas");
+	d.takeDamage(60);
+	d.beRepaired(30);
+	delete ptr;
+}
